74e0bed5098fa368469bedd36ffc15fd10f8d1f5
LouisXV16 <louis.saintmachin@gmail.com>
2024-05-21 18:01:19 +0200

    change almost all

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -16,15 +16,8 @@ def text_indentation(text):
     if not isinstance(text, str):
         raise TypeError("text must be a string")
 
-    characters = ['.', '?', ':']
-    result = ''
-    i = 0
-    while i < len(text):
-        result += text[i]
-        if text[i] in characters:
-            result += "\n\n"
-            # Skip following spaces
-            while i + 1 < len(text) and text[i + 1] == ' ':
-                i += 1
-        i += 1
-    print(result.strip())
+    for ponctuation in ".?:":
+        text = (ponctuation + "\n\n").join(
+                [line.strip(" ") for line in text.split(ponctuation)])
+
+    print("{}".format(text),end="")
