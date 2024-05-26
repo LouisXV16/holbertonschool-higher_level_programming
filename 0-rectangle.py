3af17f6cd89b01789a0bc2c6a223b87f3500b476
LouisXV16 <louis.saintmachin@gmail.com>
2024-05-26 12:15:16 +0200

    task 0

--- 0-rectangle.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-
-""" Class Rectangle that defines a rectangle"""
-
-
-class Rectangle:
-    """ Empty class that defines a rectangle"""
-    pass
--- python-classes/0-square.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-""" Class Square that defines a square"""
-
-
-class Square:
-    """ Empty class that defines a square"""
-    pass
