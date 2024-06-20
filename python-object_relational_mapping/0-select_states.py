50d6cc6ff532256cf86cf451417df525ab31fb9c
LouisXV16 <louis.saintmachin@gmail.com>
2024-06-20 19:57:30 +0200

    Refactor Python script to select and list states from a MySQL database

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,24 +1,43 @@
 #!/usr/bin/python3
-""" Script that lists all states from the database hbtn_0e_0_usa """
+import sys
 import MySQLdb
-from sys import argv
 
-# The code should not be executed when imported
 if __name__ == "__main__":
+    try:
+        # Get MySQL credentials and database name from command line arguments
+        mysql_username = sys.argv[1]
+        mysql_password = sys.argv[2]
+        database_name = sys.argv[3]
 
-    # make a connection to the database
-    db = MySQLdb.connect(
-        host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3]
-    )
+        # Connect to MySQL server on localhost at port 3306
+        db = MySQLdb.connect(
+            host="localhost",
+            user=mysql_username,
+            passwd=mysql_password,
+            db=database_name,
+            port=3306
+        )
 
-    # It gives us the ability to have multiple seperate working environments
-    # through the same connection to the database.
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states")
+        # Create a cursor object to interact with the database
+        cur = db.cursor()
 
-    rows = cur.fetchall()
-    for i in rows:
-        print(i)
-    # Clean up process
-    cur.close()
-    db.close()
+        # Execute SQL query to select all states, sorted by id in ascending order
+        cur.execute("SELECT id, name FROM states ORDER BY id ASC")
+
+        # Fetch all the results from the executed query
+        rows = cur.fetchall()
+
+        # Loop through the results and print each row
+        for row in rows:
+            print(row)
+
+        # Close the cursor and the database connection
+        cur.close()
+        db.close()
+
+    except IndexError:
+        print("Usage: ./0-select_states.py <mysql_username> <mysql_password> <database_name>")
+        sys.exit(1)
+    except MySQLdb.OperationalError as e:
+        print(f"Error: {e}")
+        sys.exit(1)
