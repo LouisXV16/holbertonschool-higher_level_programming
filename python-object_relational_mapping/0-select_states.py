fe4df363ff26c57359dee1669a10a518c33aa70a
LouisXV16 <louis.saintmachin@gmail.com>
2024-06-20 19:58:31 +0200

    refactor: Update Python script to select and list states from a MySQL database

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,43 +1,32 @@
 #!/usr/bin/python3
-import sys
+''' Script that lists all states from database hbtn_0e_0_usa '''
+
 import MySQLdb
+import sys
 
 if __name__ == "__main__":
-    try:
-        # Get MySQL credentials and database name from command line arguments
-        mysql_username = sys.argv[1]
-        mysql_password = sys.argv[2]
-        database_name = sys.argv[3]
-
-        # Connect to MySQL server on localhost at port 3306
-        db = MySQLdb.connect(
-            host="localhost",
-            user=mysql_username,
-            passwd=mysql_password,
-            db=database_name,
-            port=3306
-        )
-
-        # Create a cursor object to interact with the database
-        cur = db.cursor()
-
-        # Execute SQL query to select all states, sorted by id in ascending order
-        cur.execute("SELECT id, name FROM states ORDER BY id ASC")
-
-        # Fetch all the results from the executed query
-        rows = cur.fetchall()
-
-        # Loop through the results and print each row
-        for row in rows:
-            print(row)
-
-        # Close the cursor and the database connection
-        cur.close()
-        db.close()
-
-    except IndexError:
-        print("Usage: ./0-select_states.py <mysql_username> <mysql_password> <database_name>")
+    if len(sys.argv) != 4:
+        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
         sys.exit(1)
-    except MySQLdb.OperationalError as e:
-        print(f"Error: {e}")
-        sys.exit(1)
+
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
+
+    connection = MySQLdb.connect(host="localhost",
+                                 port=3306,
+                                 user=mysql_username,
+                                 passwd=mysql_password,
+                                 db=database_name
+                                 )
+
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+
+    states = cursor.fetchall()
+
+    for state in states:
+        print(state)
+
+    cursor.close()
+    connection.close()
